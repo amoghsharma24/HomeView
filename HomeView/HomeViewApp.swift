0da026b95b97f2cf0866648cae7832e70678aa65
Amogh Sharma <amoghsharma02@gmail.com>
2025-02-21 14:38:46 +1100

    Initial Commit

---
+++ HomeView/HomeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewApp.swift
+//  HomeView
+//
+//  Created by Amogh Sharma  on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct HomeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
